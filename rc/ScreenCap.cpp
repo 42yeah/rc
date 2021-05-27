3666c0d833b42b5681d9cbb1bff7e0c93833e8f8
42yeah's Windows <42yeah@pm.me>
2021-05-27 23:07:17 +0800

    So this works for some reason

--- rc/ScreenCap.cpp
+++ rc/ScreenCap.cpp
@@ -1,1 +1,26 @@
 #include "ScreenCap.h"
+#include <fstream>
+
+
+ScreenCap::ScreenCap() {
+	screen_dc = CreateDC(LPCWSTR(L"DISPLAY"), nullptr, nullptr, nullptr);
+	memory_dc = CreateCompatibleDC(screen_dc);
+
+	width = GetDeviceCaps(screen_dc, HORZRES);
+	height = GetDeviceCaps(screen_dc, VERTRES);
+
+	bitmap = CreateCompatibleBitmap(screen_dc, width, height);
+	old_bitmap = (HBITMAP) SelectObject(memory_dc, bitmap);
+}
+
+ScreenCap::~ScreenCap() {
+	DeleteDC(screen_dc);
+	DeleteDC(memory_dc);
+}
+
+void ScreenCap::shot() {
+	BitBlt(memory_dc, 0, 0, width, height, screen_dc, 0, 0, SRCCOPY);
+	bitmap = (HBITMAP) SelectObject(memory_dc, old_bitmap);
+
+	std::ofstream writer("out.txt");
+}
--- rc/ScreenCap.h
+++ rc/ScreenCap.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include <Windows.h>
+
 class ScreenCap {
+public:
+	ScreenCap();
+
+	~ScreenCap();
+
+	void shot();
+
+private:
+	HDC screen_dc, memory_dc;
+	int width, height;
+	HBITMAP bitmap, old_bitmap;
 };
 
